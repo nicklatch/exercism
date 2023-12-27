694ad8838aac7702bad77fec89ae35efc10942ae
Nick Latcham <nick.latcham@proton.me>
2023-12-27 06:01:52 -0600

    Refactor: working on keeping IO in one function:
    Rest of functions are working towards "pure"

--- java/grep/src/main/java/GrepTool.java
+++ java/grep/src/main/java/GrepTool.java
@@ -6,35 +6,31 @@ import java.util.List;
 
 class GrepTool {
 
-    private static String pattern;
-    private static List<String> flags;
-    private static List<String> files;
-    private static boolean isSingleton;
     private final StringBuilder grepFound = new StringBuilder();
 
 
-    String grep(String pattern, List<String> flags, List<String> files) {
-        GrepTool.pattern = flags.contains("-i") ? pattern.toLowerCase() : pattern;
-        GrepTool.files = files;
-        GrepTool.flags = flags;
-        GrepTool.isSingleton = files.size() == 1;
+    public String grep(String pattern, List<String> flags, List<String> files) {
+        String localPattern = flags.contains("-i") ? pattern.toLowerCase() : pattern;
+        boolean isSingleton = files.size() == 1;
 
-        for (String file : files) {
-            try (BufferedReader reader = Files.newBufferedReader(Paths.get(file))) {
-                if (flags.contains("-l")) {
-                    nameOnly();
-                } else {
-                    lineParsing(reader.lines().toList(), file);
+        if (flags.contains("-l")) {
+            nameOnly(files, grepFound, localPattern);
+        } else {
+            for (String file : files) {
+                try (BufferedReader reader = Files.newBufferedReader(Paths.get(file))) {
+                    lineParsing(reader.lines().toList(), file, localPattern, flags, isSingleton);
+                } catch (Exception e) {
+                    throw new RuntimeException("Hoopla", e);
                 }
-            } catch (Exception e) {
-                throw new RuntimeException("Hoopla", e);
             }
         }
+
         return grepFound.toString().trim();
+
     }
 
 
-    private void nameOnly() {
+    private void nameOnly(List<String> files, StringBuilder grepFound, String pattern) {
         for (String file : files) {
             try (BufferedReader reader = Files.newBufferedReader(Paths.get(file))) {
                 List<String> fileLines = reader.lines().toList();
@@ -57,14 +53,14 @@ class GrepTool {
     }
 
 
-    void lineParsing(List<String> fileLines, String file) {
+    private void lineParsing(List<String> fileLines, String file, String pattern, List<String> flags, Boolean isSingleton) {
         for (String line : fileLines) {
             var localLine = flags.contains("-i") ? line.toLowerCase() : line;
             var isInverted = flags.contains("-v") != (flags.contains("-x") ? localLine.equals(pattern) : localLine.contains(pattern));
 
             if (isInverted) {
                 if (flags.contains("-n")) {
-                    grepFound.append(prependLineNumber(line, fileLines.indexOf(line) + 1, file)).append("\n");
+                    grepFound.append(prependLineNumber(line, fileLines.indexOf(line) + 1, file, isSingleton)).append("\n");
                 } else {
                     grepFound.append(isSingleton ? "%s\n".formatted(line) : "%s:%s\n".formatted(file, line));
                 }
@@ -73,7 +69,7 @@ class GrepTool {
     }
 
 
-    private String prependLineNumber(String line, int lineNumber, String file) {
+    private String prependLineNumber(String line, int lineNumber, String file, boolean isSingleton) {
         return isSingleton ? "%d:%s".formatted(lineNumber, line) : "%s:%d:%s".formatted(file, lineNumber, line);
     }
 
