51d9c8118c54d51fad3fea36dcc8f4c5ec3711bd
Nick Latcham <nick.latcham@proton.me>
2023-12-26 21:52:31 -0600

    ..

--- fsharp/log-levels/LogLevels.fs
+++ fsharp/log-levels/LogLevels.fs
@@ -1,7 +1,9 @@
 module LogLevels
 
-let message (logLine: string): string = failwith "Please implement the 'message' function"
+let message (logLine: string) : string = logLine.Split(':').[1].Trim()
 
-let logLevel(logLine: string): string = failwith "Please implement the 'logLevel' function"
+let logLevel (logLine: string) : string =
+    logLine.Split(':').[0].Trim('[', ']').ToLower()
 
-let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+let reformat (logLine: string) : string =
+    $"""{message logLine} ({logLevel logLine})"""
