292937df6f8baee20038f43e91dd4690bcbc4da5
Nick Latcham <nick.latcham@proton.me>
2023-12-27 06:50:21 -0600

    all methods are now pure
    writing to the string builder is only done through the grep() method. All other methods only return either a string or list of strings and their contents are returned and appended in grep().

--- java/grep/src/main/java/GrepTool.java
+++ java/grep/src/main/java/GrepTool.java
@@ -1,23 +1,22 @@
 import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 
 class GrepTool {
-
-
     public String grep(String pattern, List<String> flags, List<String> files) {
         final StringBuilder grepFound = new StringBuilder();
         String localPattern = flags.contains("-i") ? pattern.toLowerCase() : pattern;
         boolean isSingleton = files.size() == 1;
 
         if (flags.contains("-l")) {
-            nameOnly(files, grepFound, localPattern);
+            nameOnly(files, localPattern).forEach(grepFound::append);
         } else {
             for (String file : files) {
                 try (BufferedReader reader = Files.newBufferedReader(Paths.get(file))) {
-                    lineParsing(reader.lines().toList(), file, localPattern, flags, isSingleton, grepFound);
+                    lineParsing(reader.lines().toList(), file, localPattern, flags, isSingleton).forEach(grepFound::append);
                 } catch (Exception e) {
                     throw new RuntimeException("Hoopla", e);
                 }
@@ -29,18 +28,20 @@ class GrepTool {
     }
 
 
-    private void nameOnly(List<String> files, StringBuilder grepFound, String pattern) {
+    private List<String> nameOnly(List<String> files, String pattern) {
+        List<String> filteredFileNames = new ArrayList<>();
+
         for (String file : files) {
             try (BufferedReader reader = Files.newBufferedReader(Paths.get(file))) {
                 List<String> fileLines = reader.lines().toList();
 
-                if (grepFound.toString().contains(file)) {
+                if (filteredFileNames.contains(file)) {
                     continue;
                 }
 
                 for (String line : fileLines) {
                     if (line.contains(pattern)) {
-                        grepFound.append(file).append("\n");
+                        filteredFileNames.add(file + "\n");
                         break;
                     }
                 }
@@ -48,33 +49,27 @@ class GrepTool {
                 throw new RuntimeException("Hoopla", e);
             }
         }
+        return filteredFileNames;
 
     }
 
 
-    private void lineParsing(
-            List<String> fileLines,
-            String file,
-            String pattern,
-            List<String> flags,
-            Boolean isSingleton,
-            StringBuilder grepFound
-    ) {
-        for (String line : fileLines) {
-            var localLine = flags.contains("-i") ? line.toLowerCase() : line;
+    private List<String> lineParsing(List<String> fileLines, String file, String pattern, List<String> flags, Boolean isSingleton) {
+        List<String> filteredLines = new ArrayList<>();
 
-            var isInverted = flags.contains("-v") != (flags.contains("-x")
-                    ? localLine.equals(pattern)
-                    : localLine.contains(pattern));
+        for (String line : fileLines) {
+            String localLine = flags.contains("-i") ? line.toLowerCase() : line;
+            boolean isInverted = flags.contains("-v") != (flags.contains("-x") ? localLine.equals(pattern) : localLine.contains(pattern));
 
             if (isInverted) {
                 if (flags.contains("-n")) {
-                    grepFound.append(prependLineNumber(line, fileLines.indexOf(line) + 1, file, isSingleton)).append("\n");
+                    filteredLines.add(prependLineNumber(line, fileLines.indexOf(line) + 1, file, isSingleton) + "\n");
                 } else {
-                    grepFound.append(isSingleton ? "%s\n".formatted(line) : "%s:%s\n".formatted(file, line));
+                    filteredLines.add(isSingleton ? "%s\n".formatted(line) : "%s:%s\n".formatted(file, line));
                 }
             }
         }
+        return filteredLines;
     }
 
 
