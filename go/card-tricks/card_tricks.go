8076a340e7706f625fed0e36099551947a51e2ab
Nick <nick.latcham@proton.me>
2024-02-13 16:22:01 -0600

    ..

---
+++ go/card-tricks/card_tricks.go
@@ -0,0 +1,43 @@
+package cards
+
+import "slices"
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int {
+	return []int{2, 6, 9}
+}
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+	if index >= 0 && index < len(slice) {
+		return slice[index]
+	} else {
+		return -1
+	}
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if index >= 0 && index < len(slice) {
+		slice[index] = value
+		return slice
+	} else {
+		return append(slice, value)
+	}
+}
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(slice []int, values ...int) []int {
+	return append(values, slice...)
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if index >= 0 && index < len(slice) {
+		return slices.Delete(slice, index, index+1)
+	} else {
+		return slice
+	}
+}
