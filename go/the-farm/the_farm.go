b08932c3a5ddad6349c17ebb97ea93eb9acdf63b
Nick Latcham <nick.latcham@proton.me>
2024-02-19 20:25:36 -0600

    feat: complete the-farm

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -1,46 +1,52 @@
 package thefarm
 
 import (
+	"errors"
 	"fmt"
 )
-
-// TODO: define the 'DivideFood' function
-func DivideFood(fc FodderCalculator, cowQty int) (float64, error) {
-
-	panic("")
-}
-
-// TODO: define the 'ValidateInputAndDivideFood' function
-func ValidateInputAndDivideFood(fc FodderCalculator, cowQty int) (float64, error) {
-	// check if cowQty > 0, if so call DivideFood with params
-	// if lt 0, return error msg of "invalid number of cows"
-	panic("")
-}
 
 type InvalidCowsError struct {
 	cowQty int
 	msg    string
 }
 
+// Error returns a formated error message from an InvalidCowsError
 func (e InvalidCowsError) Error() string {
 	return fmt.Sprintf("%d cows are invalid: %s", e.cowQty, e.msg)
 }
 
-// TODO: define the 'ValidateNumberOfCows' function
+// DivideFood accepts a FodderCalculator and int representing the quantity
+// of cows and returns the diveded food amount and an err (if available, else nil)
+func DivideFood(fc FodderCalculator, cowQty int) (float64, error) {
+	totalFodder, err := fc.FodderAmount(cowQty)
+	if err != nil {
+		return 0.0, err
+	}
+
+	fatFac, err := fc.FatteningFactor()
+	if err != nil {
+		return 0.0, err
+	}
+	return totalFodder / float64(cowQty) * fatFac, nil
+}
+
+// ValidateInputAndDivideFood uses the ValidateNumberOfCows func to either return an
+// error or return the value of a call to DivideFood with its given params
+func ValidateInputAndDivideFood(fc FodderCalculator, cowQty int) (float64, error) {
+	err := ValidateNumberOfCows(cowQty)
+	if err != nil {
+		return 0.0, errors.New("invalid number of cows")
+	}
+	return DivideFood(fc, cowQty)
+}
+
+// ValidateNumberOfCows returns an error messagme based on the cowQty param, or nil
+// if cowQty is valid
 func ValidateNumberOfCows(cowQty int) error {
 	if cowQty < 0 {
-		return &InvalidCowsError{cowQty: cowQty, msg: "invalid number of cows"}
+		return &InvalidCowsError{cowQty: cowQty, msg: "there are no negative cows"}
 	} else if cowQty == 0 {
-		return &InvalidCowsError{cowQty: cowQty, msg: "now cows dont need food"}
+		return &InvalidCowsError{cowQty: cowQty, msg: "no cows don't need food"}
 	}
 	return nil
 }
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
