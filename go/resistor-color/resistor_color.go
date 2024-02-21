33dfff6b4945a76d0d499386bee01e91e2b5aadc
Nick <nick.latcham@proton.me>
2024-02-21 16:33:06 -0600

    asdf

--- go/resistor-color/resistor_color.go
+++ go/resistor-color/resistor_color.go
@@ -1,37 +1,35 @@
 package resistorcolor
 
-import "slices"
-
-type Color struct {
-	name  string
-	value int
-}
+import (
+	"slices"
+	"strings"
+)
 
 // incomplete
-var colors = [10]Color{
-	Color{name: "Black", value: 0},
-	Color{name: "Brown", value: 1},
-	Color{name: "Red", value: 2},
-	Color{name: "Orange", value: 3},
-	Color{name: "Yellow", value: 4},
-	Color{name: "Green", value: 5},
-	Color{name: "Blue", value: 6},
-	Color{name: "Violet", value: 7},
-	Color{name: "Grey", value: 8},
-	Color{name: "White", value: 9},
+var colors = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
 }
 
 // Colors returns the list of all colors.
 func Colors() []string {
 	var colorList []string
 	slices.Grow(colorList, 10)
-	for _, color := range colors {
-		colorList = append(colorList, color.name)
+	for name, _ := range colors {
+		colorList = append(colorList, name)
 	}
 	return colorList
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-
+	return colors[strings.ToLower(color)]
 }
