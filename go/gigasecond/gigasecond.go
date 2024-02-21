f5315fad35e40e3e070d73e03e383914b800f397
Nick <nick.latcham@proton.me>
2024-02-21 10:59:22 -0600

    feat: complete gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,18 +1,17 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package gigasecond
 
-// import path for the time package from the standard library
-import "time"
+import (
+	"log"
+	"time"
+)
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond accepts a param "t" of time.Time and returns
+// a "t" plus one gigasecond
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	var gigasecond, err = time.ParseDuration("1000000000s")
+	if err != nil {
+		log.Println(err)
+		return t
+	}
+	return t.Add(gigasecond)
 }
