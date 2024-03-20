85dc043f242d63c3bdd9bde5d172d036a2dc71da
Nick <nick.latcham@proton.me>
2024-03-20 13:15:36 -0500

    ..

--- zig/isbn-verifier/isbn_verifier.zig
+++ zig/isbn-verifier/isbn_verifier.zig
@@ -1,4 +1,28 @@
+const std = @import("std");
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+/// Returns a bool dependant on if `s` is a valid ISBN
 pub fn isValidIsbn10(s: []const u8) bool {
-    _ = s;
-    @compileError("please implement the isValidIsbn10 function");
+    const length = s.len;
+    if (length < 10 or length > 13) return false;
+
+    var multiplier: u8 = 10;
+    var sum: u32 = 0;
+    
+    for (s) |c| {
+        var digit: u8 = undefined;
+        switch (c) {
+            '-' => continue,
+            '0'...'9' => digit = c - '0',
+            'X' => digit = 10,
+            else => return false,
+        }
+
+        if (multiplier < 1 or digit == 10 and multiplier > 1) return false;
+
+        sum += (digit * multiplier);
+        multiplier -= 1;
+    }
+
+    return multiplier == 0 and sum % 11 == 0;
 }
