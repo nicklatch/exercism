2542949da03d9f27950af909929da8bf35aa629d
Nick <nick.latcham@proton.me>
2024-03-21 10:55:55 -0500

    completed luhn

--- zig/luhn/luhn.zig
+++ zig/luhn/luhn.zig
@@ -1,30 +1,33 @@
-pub fn isValid(s: []const u8) bool {
-    const s_len = s.len;
-    var checksum: u32 = 0;
-    var num_count: u32 = 0;
+const std = @import("std");
 
-    for (s, 0..) |_, idx| {
-        const val = s[s_len - 1 - idx];
-        switch (val) {
-            '0'...'9' => {
-                const n = val - '0';
-                switch (@mod(num_count, 2) != 0) {
-                    true => {
-                        checksum += switch (n >= 5) {
-                            true => n * 2 - 9,
-                            false => n * 2,
-                        };
-                    },
-                    false => {
-                        checksum += n;
-                        num_count += 1;
-                    },
-                }
+pub fn isValid(s: []const u8) bool {
+    switch (s.len) { // avoid un-needed allocations with early return
+        1 => return false,
+        2 => if (s[0] == ' ' or s[1] == ' ') return false,
+        else => {},
+    }
+
+    var checksum: u32 = 0;
+    var double_if_odd: u32 = 0;
+    var val: u32 = undefined; // declared outside of loop to avoid allocation on each iteration
+    var current: u8 = undefined; // declared outside of loop to avoid allocation on each iteration
+    var idx: usize = s.len - 1;
+
+    while (idx >= 0) : (idx -= 1) { // chose to go with a while loop here to avoid having to reverse the input.
+        if (s[idx] == ' ') continue;
+
+        current = s[idx];
+        switch (current >= '0' and current <= '9') {
+            true => {
+                val = current - '0';
+                if (double_if_odd % 2 == 1) val <<= 1;
+                if (val > 9) val -= 9;
+                checksum += val;
+                double_if_odd += 1;
+                if (idx == 0) break; // prevents overflow on final iteration
             },
-            ' ' => continue,
             else => return false,
         }
     }
-
-    return @mod(checksum, 10) == 0 and num_count > 1;
+    return checksum % 10 == 0;
 }
