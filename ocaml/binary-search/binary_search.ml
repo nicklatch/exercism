48e5b9ad5826da5816b0aa82fa9f0580df147ba8
Nick <96550850+nicklatch@users.noreply.github.com>
2024-03-26 15:08:15 +0000

    refactor for readability

--- ocaml/binary-search/binary_search.ml
+++ ocaml/binary-search/binary_search.ml
@@ -1,10 +1,14 @@
 let find arr key =
-    if Array.length arr = 0 then Error "value not in array" else
-    let rec find' left right =
-        match (left + right) / 2 with
-        | _ when left > right -> Error "value not in array"
-        | res when key = arr.(res) -> Ok res
-        | res when key < arr.(res) -> find' left (res - 1)
-        | res -> find' (res + 1) right
-    in
-    find' 0 (Array.length arr - 1)
+    let err = Error "value not in array" in
+    match arr with
+    | [||] -> err
+    | [| v |] -> if v = key then Ok 0 else err
+    | arr ->
+        let rec search left right =
+            match (left + right) / 2 with
+            | _ when left > right -> err
+            | res when key = arr.(res) -> Ok res
+            | res when key < arr.(res) -> search left (res - 1)
+            | res -> search (res + 1) right
+        in
+        search 0 (Array.length arr - 1)
