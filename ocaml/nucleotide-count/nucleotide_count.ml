922a52b43a2ec56a6f02fac2c16afa2a1a1e927c
Nick Latcham <nick.latcham@proton.me>
2024-03-28 15:43:12 -0500

    ...

--- ocaml/nucleotide-count/nucleotide_count.ml
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -1,17 +1,15 @@
 open Base
 
 let empty = Map.empty (module Char)
-
 let valid_nucl = function 'A' | 'C' | 'G' | 'T' -> true | _ -> false
 
 let count_nucleotide s c =
-    let rec count l sum =
+  let rec count l sum =
     match l with
     | [] -> Error "X"
-    | [x] -> if x = c then Ok (sum + 1, c) else Ok (sum, c)
+    | [ x ] -> if x = c then Ok (sum + 1, c) else Ok (sum, c)
     | h :: t -> if h = c then count t (sum + 1) else count t sum
   in
   count s 0
 
-let count_nucleotides s =
-  failwith "'count_nucleotides' is missing"
+let count_nucleotides s = failwith "'count_nucleotides' is missing"
