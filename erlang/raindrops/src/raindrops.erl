eb3e3caecad49414dadc166a73d98f23be887346
Nick Latcham <nick.latcham@proton.me>
2024-03-28 15:50:21 -0500

    ..

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -2,5 +2,14 @@
 
 -export([convert/1]).
 
-
-convert(_Number) -> undefined.
+convert(Number) ->
+    case {Number rem 3, Number rem 5, Number rem 7} of
+        {0, 0, 0} -> "PlingPlangPlong";
+        {0, 0, _} -> "PlingPlang";
+        {0, _, 0} -> "PlingPlong";
+        {_, 0, 0} -> "PlangPlong";
+        {0, _, _} -> "Pling";
+        {_, 0, _} -> "Plang";
+        {_, _, 0} -> "Plong";
+        _ -> integer_to_list(Number)
+    end.
