78b3789b0374cd68ef34929f7c12d6eda9ec4811
Nick <96550850+nicklatch@users.noreply.github.com>
2024-03-29 17:58:33 +0000

    complete nuc count

--- ocaml/nucleotide-count/nucleotide_count.mli
+++ ocaml/nucleotide-count/nucleotide_count.mli
@@ -1,13 +1,9 @@
 open Base
 
+val empty : (char, 'a, Base.Char.comparator_witness) Base.Map.t
+
 (* Count the number of times a nucleotide occurs in the string. *)
-val count_nucleotidval empty : (char, 'a, Base.Char.comparator_witness) Base.Map.t
-
-val count_nucleotide :
-  'a -> int -> int -> (int * int, string) result
-
-val count_nucleotides : 'a -> 'b
-e : string -> char -> (int, char) Result.t
+val count_nucleotide : string -> char -> (int, char) Result.t
 
 (* Count the nucleotides in the string. *)
 val count_nucleotides : string -> (int Map.M(Char).t, char) Result.t
