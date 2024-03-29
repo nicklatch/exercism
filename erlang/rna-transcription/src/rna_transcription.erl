e440f632fcb794c29cc17fd88aa5238809514d22
Nick Latcham <nick.latcham@proton.me>
2024-03-28 20:17:46 -0500

    complete rna

--- erlang/rna-transcription/src/rna_transcription.erl
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -2,5 +2,10 @@
 
 -export([to_rna/1]).
 
+to_rna(_Strand) ->
+    lists:map(fun from_dna/1, _Strand).
 
-to_rna(_Strand) -> undefined.
+from_dna($G) -> $C;
+from_dna($C) -> $G;
+from_dna($T) -> $A;
+from_dna($A) -> $U.
