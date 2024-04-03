aeb223324d54885ba24bc5f83026019bfc1f1673
Nick <96550850+nicklatch@users.noreply.github.com>
2024-04-03 18:09:26 +0000

    refactor: change tuple args to patter

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,11 +1,7 @@
 defmodule KitchenCalculator do
-  def get_volume(volume_pair) do
-    elem(volume_pair, 1)
-  end
+  def get_volume({_, vol}), do: vol
 
-  def to_milliliter(volume_pair) do
-    {unit, vol} = volume_pair
-
+  def to_milliliter({unit, vol}) do
     case unit do
       :cup -> {:milliliter, vol * 240}
       :fluid_ounce -> {:milliliter, vol * 30}
@@ -15,9 +11,7 @@ defmodule KitchenCalculator do
     end
   end
 
-  def from_milliliter(volume_pair, unit) do
-    {_, vol} = volume_pair
-
+  def from_milliliter({_, vol}, unit) do
     case unit do
       :cup -> {:cup, vol / 240}
       :fluid_ounce -> {:fluid_ounce, vol / 30}
