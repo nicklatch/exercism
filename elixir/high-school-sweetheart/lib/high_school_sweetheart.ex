cccf1f46c5f4933caef128420cc3354a260a392f
Nick <96550850+nicklatch@users.noreply.github.com>
2024-04-04 14:28:03 +0000

    refactor: add type specs

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,15 +1,19 @@
 defmodule HighSchoolSweetheart do
+  @spec first_letter(String.t()) :: String.t()
   def first_letter(name) do
     name |> String.replace(["\s", "\n", "\r", "\t"], "") |> String.first()
   end
 
+  @spec initial(Strting.t()) :: String.t()
   def initial(name), do: first_letter(String.upcase(name)) <> "."
 
+  @spec initials(Strting.t()) :: String.t()
   def initials(full_name) do
     [first_name, last_name] = String.split(full_name)
     initial(first_name) <> " " <> initial(last_name)
   end
 
+  @spec pair(String.t(), String.t()) :: String.t()
   def pair(full_name1, full_name2) do
     """
          ******       ******
