ef17af01f9f8c5c34b676eb4b12e4652e263c464
Nick <96550850+nicklatch@users.noreply.github.com>
2024-04-04 15:11:43 +0000

    refactor: make recursively called functions defp

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -10,12 +10,12 @@ defmodule BirdCount do
   def has_day_without_birds?([head | _tail]) when head == 0, do: true
 
   def total(list), do: total(list, 0)
-  def total([], initial), do: initial
-  def total([head | tail], initial), do: total(tail, head + initial)
+  defp total([], initial), do: initial
+  defp total([head | tail], initial), do: total(tail, head + initial)
 
   def busy_days(list), do: busy_days(list, 0)
-  def busy_days([], initial), do: initial
-  def busy_days([day], initial) when day >= 5, do: initial + 1
-  def busy_days([head | tail], initial) when head >= 5, do: busy_days(tail, initial + 1)
-  def busy_days([_head | tail], initial), do: busy_days(tail, initial)
+  defp busy_days([], initial), do: initial
+  defp busy_days([day], initial) when day >= 5, do: initial + 1
+  defp busy_days([head | tail], initial) when head >= 5, do: busy_days(tail, initial + 1)
+  defp busy_days([_head | tail], initial), do: busy_days(tail, initial)
 end
