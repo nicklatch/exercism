5e23f8525a71e0c495deece06cf9892d0b069e19
Nick <96550850+nicklatch@users.noreply.github.com>
2024-04-05 15:23:03 +0000

    refactor: create constant and refactor age_on
    earth_divisor now holds all divisors for age_on's calculations. This
    prompts the remove of redundant function pattern match cases. Makes code
    more consise and easier to read.

--- elixir/space-age/lib/space_age.ex
+++ elixir/space-age/lib/space_age.ex
@@ -9,20 +9,30 @@ defmodule SpaceAge do
           | :uranus
           | :neptune
 
+  @earth_divisor %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  # A helper function the turns the given seconds into age on earth in years
+  @spec s_to_y(seconds :: float()) :: float()
   defp s_to_y(seconds), do: seconds / 31_557_600
 
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet', or an error if 'planet' is not a planet.
   """
-  @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
-  def age_on(planet, seconds) when planet == :mercury, do: {:ok, s_to_y(seconds) / 0.2408467}
-  def age_on(planet, seconds) when planet == :venus, do: {:ok, s_to_y(seconds) / 0.61519726}
-  def age_on(planet, seconds) when planet == :earth, do: {:ok, s_to_y(seconds)}
-  def age_on(planet, seconds) when planet == :mars, do: {:ok, s_to_y(seconds) / 1.8808158}
-  def age_on(planet, seconds) when planet == :jupiter, do: {:ok, s_to_y(seconds) / 11.862615}
-  def age_on(planet, seconds) when planet == :saturn, do: {:ok, s_to_y(seconds) / 29.447498}
-  def age_on(planet, seconds) when planet == :uranus, do: {:ok, s_to_y(seconds) / 84.016846}
-  def age_on(planet, seconds) when planet == :neptune, do: {:ok, s_to_y(seconds) / 164.79132}
+  @spec age_on(planet :: planet(), seconds :: pos_integer) :: {:ok, float} | {:error, String.t()}
+  def age_on(:earth, seconds), do: {:ok, s_to_y(seconds)}
+
+  def age_on(planet, seconds) when is_map_key(@earth_divisor, planet) do
+    {:ok, s_to_y(seconds) / @earth_divisor[planet]}
+  end
+
   def age_on(_planet, _seconds), do: {:error, "not a planet"}
 end
