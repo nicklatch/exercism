833a4d58e28a805423d9f4127976e67561697c49
Nick <96550850+nicklatch@users.noreply.github.com>
2024-04-05 18:15:25 +0000

    chore: formatting

--- erlang/list-ops/src/list_ops.erl
+++ erlang/list-ops/src/list_ops.erl
@@ -2,16 +2,7 @@
 
 -compile({no_auto_import, [length/1]}).
 
--export([
-    append/2,
-    concat/1,
-    filter/2,
-    length/1,
-    map/2,
-    foldl/3,
-    foldr/3,
-    reverse/1
-]).
+-export([append/2, concat/1, filter/2, length/1, map/2, foldl/3, foldr/3, reverse/1]).
 
 append(T1, T2) ->
     case {T1, T2} of
@@ -26,8 +17,10 @@ append(T1, T2) ->
 
 concat(List) ->
     case List of
-        [] -> [];
-        [H | T] -> append(H, concat(T))
+        [] ->
+            [];
+        [H | T] ->
+            append(H, concat(T))
     end.
 
 % Keeping these two seperate for readability
@@ -35,42 +28,56 @@ filter(_, []) ->
     [];
 filter(Function, [H | T]) ->
     case Function(H) of
-        true -> [H | filter(Function, T)];
-        false -> filter(Function, T)
+        true ->
+            [H | filter(Function, T)];
+        false ->
+            filter(Function, T)
     end.
 
 length(List) ->
     case List of
-        [] -> 0;
-        [_ | T] -> 1 + length(T)
+        [] ->
+            0;
+        [_ | T] ->
+            1 + length(T)
     end.
 
 map(Function, List) ->
     case List of
-        [] -> [];
-        [H | T] -> [Function(H) | map(Function, T)]
+        [] ->
+            [];
+        [H | T] ->
+            [Function(H) | map(Function, T)]
     end.
 
 foldl(Function, Start, List) ->
     case List of
-        [] -> Start;
-        [H | T] -> foldl(Function, Function(H, Start), T)
+        [] ->
+            Start;
+        [H | T] ->
+            foldl(Function, Function(H, Start), T)
     end.
 
 foldr(Function, Start, List) ->
     case List of
-        [] -> Start;
-        _ -> foldl(Function, Start, reverse(List))
+        [] ->
+            Start;
+        _ ->
+            foldl(Function, Start, reverse(List))
     end.
 
 reverse(List) ->
     case List of
-        [] -> [];
-        _ -> reverse(List, [])
+        [] ->
+            [];
+        _ ->
+            reverse(List, [])
     end.
 
 reverse(List, Acc) ->
     case List of
-        [] -> Acc;
-        [H | T] -> reverse(T, [H | Acc])
+        [] ->
+            Acc;
+        [H | T] ->
+            reverse(T, [H | Acc])
     end.
