256c2c93102bd9ec9580b2e85c1c1608cc02f6c8
Nick Latcham <nick.latcham@proton.me>
2024-09-25 21:48:07 -0500

    crystal/crystal-hunter: complete

---
+++ crystal/crystal-hunter/src/crystal_hunter.cr
@@ -0,0 +1,21 @@
+class Rules
+  def bonus_points?(power_up_active, touching_bandit)
+    power_up_active && touching_bandit
+  end
+
+  def score?(touching_power_up, touching_crystal)
+    touching_power_up || touching_crystal
+  end
+
+  def lose?(power_up_active, touching_bandit)
+    !power_up_active && touching_bandit
+  end
+
+  def win?(has_picked_up_all_crystals, power_up_active, touching_bandit)
+    if has_picked_up_all_crystals
+      !lose?(power_up_active, touching_bandit)
+    else
+      false
+    end
+  end
+end
