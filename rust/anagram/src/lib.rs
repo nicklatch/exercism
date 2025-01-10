d64e30e816fb85c8bf59194571f191beb896dfd9
Nick Latcham <nick.latcham@proton.me>
2025-01-10 12:28:58 -0600

    rust/anagram: complete

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,17 +1,18 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    let mut result = HashSet::new();
-    let mut sword = word.clone().to_ascii_lowercase().split("");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let mut answer: HashSet<&str> = HashSet::new();
+    let mut sorted_word: Vec<char> = word.to_lowercase().chars().collect();
+    let _ = &sorted_word.sort();
 
-    for pos in possible_anagrams {
-        let possible = pos.to_ascii_lowercase().as_bytes();
-        possible.sort();
+    for &possible in possible_anagrams {
+        let mut sorted_possible: Vec<char> = possible.to_lowercase().chars().collect();
+        let _ = &sorted_possible.sort();
 
-        if possible == sword {
-            result.insert(*pos);
+        if sorted_possible == sorted_word && word.to_lowercase() != possible.to_lowercase() {
+            answer.insert(possible);
         }
     }
 
-    result
+    answer
 }
